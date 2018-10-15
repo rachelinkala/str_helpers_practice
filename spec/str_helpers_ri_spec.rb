eb2c9b57ff6db76567edea9b0a1670cc7338c895
Rachel Inkala <rachelinkala@gmail.com>
2018-10-15 10:58:31 -0600

    fixed spec errors

--- spec/str_helpers_ri_spec.rb
+++ spec/str_helpers_ri_spec.rb
@@ -5,14 +5,16 @@ RSpec.describe StrHelpersRi do
     expect(StrHelpersRi::VERSION).not_to be nil
   end
 
-  defscribe 'manipulate strings' do
+  describe 'manipulate strings' do
     let(:str) { 'My String' }
 
     it 'reverses a string' do
-      expect(subject.reversify(str)).to eq('gnirts yM')
+      expect(subject.reversify(str)).to eq('gnirtS yM')
     end
 
     it 'adds spaces' do
-      expect(subject.spacify(str, 1)).to eq('M y  S t r i n g')
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
     end
+  end
 end
+
